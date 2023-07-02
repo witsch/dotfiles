4ad9ad9de1a17ddb92ae11db709a86d93726742a
Andreas Zeidler <az@zitc.de>
2023-07-02 18:34:13 +0200

    Declare variables to be local

--- dot_config/private_fish/private_functions/working_time.fish
+++ dot_config/private_fish/private_functions/working_time.fish
@@ -1,8 +1,8 @@
 function working_time
-    set source ~/Sync/arbeitszeiten.txt
-    set update ~/.config/fish/functions/duration.py
+    set -l source ~/Sync/arbeitszeiten.txt
+    set -l update ~/.config/fish/functions/duration.py
 
-    set now ( date +%H:%M )
+    set -l now ( date +%H:%M )
     test "$working_time_last" != "$now"
     and set -U working_time_duration ( tail -20 $source | python3.9 $update )
     and set -U working_time_last $now

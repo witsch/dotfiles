7fc44a36e76089aa6a06195d0a4100bf56961329
Andreas Zeidler <az@zitc.de>
2024-02-22 19:29:06 +0100

    Use gentler colors for the visual divider

--- dot_config/private_fish/private_functions/divider.fish
+++ dot_config/private_fish/private_functions/divider.fish
@@ -1,4 +1,4 @@
 function divider -d "Display a visual divider line with the given text"
     set -l text ( string pad --width $COLUMNS --right --char = "===== $argv " )
-    echo -e "\e[48;2;200;255;200m$text\e[0m"
+    echo -e "\e[48;2;23;90;23m$text\e[0m"
 end

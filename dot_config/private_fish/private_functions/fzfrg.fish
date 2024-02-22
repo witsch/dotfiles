c59369b318b06231f8ce334ada0f62230ee70c17
Andreas Zeidler <az@zitc.de>
2024-02-22 16:43:46 +0100

    Refactor `fzf` integration and add `fzfrg` search
    See https://github.com/junegunn/fzf
    and https://github.com/gazorby/fifc
    and https://github.com/patrickf1/fzf.fish
    and https://github.com/junegunn/fzf/issues/2789

---
+++ dot_config/private_fish/private_functions/fzfrg.fish
@@ -0,0 +1,7 @@
+function fzfrg --description "search using rg, fzf and bat"
+    rg --with-filename --line-number --color=always --smart-case "$argv" |
+        fzf --delimiter ':' --no-sort \
+            --preview "bat --number --color always --highlight-line {2} {1}" \
+            --preview-window 'right:+{2}-10' \
+            --bind "enter:become(vim +{2} {1}),ctrl-/:change-preview-window(hidden|)'"
+end

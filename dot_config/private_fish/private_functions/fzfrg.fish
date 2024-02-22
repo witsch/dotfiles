9da0e896a6c508daf13900d408a90a8365c57ffb
Andreas Zeidler <az@zitc.de>
2024-02-22 19:26:46 +0100

    Use "exact" mode by default for ripgrep searches
    Also fix quoting.

--- dot_config/private_fish/private_functions/fzfrg.fish
+++ dot_config/private_fish/private_functions/fzfrg.fish
@@ -1,7 +1,7 @@
 function fzfrg --description "search using rg, fzf and bat"
     rg --with-filename --line-number --color=always --smart-case "$argv" |
-        fzf --delimiter ':' --no-sort \
+        fzf --delimiter ':' --no-sort --exact \
             --preview "bat --number --color always --highlight-line {2} {1}" \
             --preview-window 'right:+{2}-10' \
-            --bind "enter:become(vim +{2} {1}),ctrl-/:change-preview-window(hidden|)'"
+            --bind 'enter:become(vim +{2} {1}),ctrl-/:change-preview-window(hidden|)'
 end

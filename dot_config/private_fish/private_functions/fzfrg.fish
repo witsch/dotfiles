71b9671417a0e866eefb7b0b412f3e509ea7aa36
Andreas Zeidler <az@zitc.de>
2024-03-20 21:58:21 +0100

    Increase maximum file size when searching via `ripgrep` & `fzf`

--- dot_config/private_fish/private_functions/fzfrg.fish
+++ dot_config/private_fish/private_functions/fzfrg.fish
@@ -1,5 +1,5 @@
 function fzfrg --description "search using rg, fzf and bat"
-    rg --with-filename --line-number --color always --smart-case --hidden --max-filesize 50K "$argv" |
+    rg --with-filename --line-number --color always --smart-case --hidden --max-filesize 100K "$argv" |
         fzf --delimiter ':' --no-sort --exact \
             --preview "bat --number --color always --highlight-line {2} {1}" \
             --preview-window 'right:+{2}-10' \

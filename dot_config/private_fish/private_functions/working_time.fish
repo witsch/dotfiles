e62ca7393e653785a375eb6b163718dbc3df883c
Andreas Zeidler <az@zitc.de>
2024-03-29 13:09:57 +0100

    Don't depend on a specific Python version for showing work times

--- dot_config/private_fish/private_functions/working_time.fish
+++ dot_config/private_fish/private_functions/working_time.fish
@@ -4,7 +4,7 @@ function working_time
 
     set -l now ( date +%H:%M )
     test "$working_time_last" != "$now"
-    and set -U working_time_duration ( tail -20 $source | python3.9 $update )
+    and set -U working_time_duration ( tail -20 $source | python3 $update )
     and set -U working_time_last $now
     echo $working_time_duration
 end
